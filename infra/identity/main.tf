fd11ef868b24ec3c8f9d620048da4b33c5545550
Jacob Johnson <jacob.johnson@3cloudsolutions.com>
2025-09-03 13:43:27 -0500

    Push changes

--- infra/identity/main.tf
+++ infra/identity/main.tf
@@ -11,19 +11,19 @@ resource "azuread_service_principal" "github_actions" {
 resource "azuread_service_principal_password" "github_actions" {
   service_principal_id = azuread_service_principal.github_actions.id
   display_name         = "GitHub Actions Password"
-  end_date            = timeadd(timestamp(), "24h")  # Expires in 24 hours
+  end_date            = timeadd(timestamp(), "2h")  # Expires in 2 hours
 }
 
-# Role assignments for the service principal
-resource "azurerm_role_assignment" "github_actions_contributor" {
+# Least privilege role assignments for the service principal
+resource "azurerm_role_assignment" "github_actions_acr_push" {
   scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
-  role_definition_name = "Contributor"
+  role_definition_name = "AcrPush"
   principal_id         = azuread_service_principal.github_actions.object_id
 }
 
-resource "azurerm_role_assignment" "github_actions_key_vault_admin" {
+resource "azurerm_role_assignment" "github_actions_key_vault_certificates_officer" {
   scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
-  role_definition_name = "Key Vault Administrator"
+  role_definition_name = "Key Vault Certificates Officer"
   principal_id         = azuread_service_principal.github_actions.object_id
 }
 
